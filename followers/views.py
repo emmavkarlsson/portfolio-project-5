b9ebeb877ac6f3c53cb1903d96008e17ff9ad761
Emma Karlsson <emma94k@gmail.com>
2023-10-28 15:45:37 +0000

    create views for FollowerList and FollowerDetail and update urls

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers or create a follower if logged in.
+    Perform_create: associate the current logged in user with a follower.
+    """
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower or destroy, meaning unfollow someone if owner
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowSerializer
+    queryset = Follower.objects.all()
