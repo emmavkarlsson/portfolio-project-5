17cc027ea5f597fcf6f015ce0b4899daf368fa3f
Emma Karlsson <emma94k@gmail.com>
2023-11-06 16:18:33 +0000

    set up route for navbar links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import Button from "react-bootstrap/Button";
 import styles from './App.module.css';
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
        <NavBar />
+       <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>}></Route>
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>}></Route>
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>}></Route>
+          <Route render={() => <p>Page not found!</p>}></Route>
+        </Switch>
+      </Container>
     </div>
   );
 }
