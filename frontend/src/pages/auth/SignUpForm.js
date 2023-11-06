63d1ac5dc5f1a8b2294f220e452b6a7ec515a969
Emma Karlsson <emma94k@gmail.com>
2023-11-06 22:46:03 +0000

    add error alerts for non field errors

--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -104,6 +104,11 @@ const SignUpForm = () => {
                         >
                             Sign up
                         </Button>
+                        {errors.non_field_errors?.map((message, idx) => (
+                            <Alert key={idx} variant="warning" className="mt-3">
+                                {message}
+                            </Alert>
+                        ))}
                     </Form>
 
                 </Container>
