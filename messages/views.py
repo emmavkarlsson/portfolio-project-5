eefede7cd3311adbb329e08d25bce1e02c494e49
Emma Karlsson <emma94k@gmail.com>
2023-11-10 17:18:39 +0000

    create view for messages

--- messages/views.py
+++ messages/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Message
+from .serializers import MessageSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
+from drf_api.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+class MessageListCreateView(generics.ListCreateAPIView):
+    """
+    Let's users create a message
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class MessageDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Let's users update or delete their message
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+
+
+
