30bfbc5df9db45a24dfc7201adaab21142c65676
Emma Karlsson <emma94k@gmail.com>
2023-11-10 17:07:30 +0000

    implement messages model

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    owner = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.owner} to {self.receiver}: {self.content}'
