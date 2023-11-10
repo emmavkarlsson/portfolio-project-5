aa706133fac069050dc1ba47a01cf94e04b445a4
Emma Karlsson <emma94k@gmail.com>
2023-11-10 18:01:12 +0000

    delete messages app

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
     path('api/', include('posts.urls')),
     path('api/', include('comments.urls')),
     path('api/', include('likes.urls')),
-    path('api/', include('followers.urls')),
-    path('api/', include('messages.urls')),
+    path('api/', include('followers.urls'))
 ]
 handler404 = TemplateView.as_view(template_name='index.html')
--- messages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from messages import views
-
-urlpatterns = [
-    path('messages/', views.MessageListCreateView.as_view()),
-    path('messages/<int:pk>/', views.MessageDetailView.as_view()),
-]
