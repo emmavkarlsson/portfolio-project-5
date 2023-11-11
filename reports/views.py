011d6f6daa3eb992a77ac18478ca1df8ef605949
Emma Karlsson <emma94k@gmail.com>
2023-11-11 21:48:55 +0000

    create view for reports

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics, permissions
+from .models import Reports
+from .serializers import ReportsSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class ReportsViewSet(generics.ListCreateAPIView):
+    serializer_class = ReportsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return Reports.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ReportsDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ReportsSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Reports.objects.all()
