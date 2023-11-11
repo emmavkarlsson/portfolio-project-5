a9960e2739886e24b43034351b8f2dcf04d5cbd6
Emma Karlsson <emma94k@gmail.com>
2023-11-11 10:35:15 +0000

    change back url in drf_api

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('api/', include('likes.urls')),
     path('api/', include('followers.urls')),
     path('api/', include('usermessages.urls')),
-    path('api/', include('saved_posts.urls')),
+    path('api/', include('saved.urls')),
 ]
 handler404 = TemplateView.as_view(template_name='index.html')
