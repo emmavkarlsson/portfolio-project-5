c944aefaef2fccbe2bd734dfda8ccd2860b333e2
Emma Karlsson <emma94k@gmail.com>
2023-11-11 16:28:24 +0000

    add cover image field to profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -28,6 +28,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner', 'following_id',
+            'content', 'image','cover_image', 'is_owner', 'following_id',
             'posts_count', 'followers_count', 'following_count'
         ]
