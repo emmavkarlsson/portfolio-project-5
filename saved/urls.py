211cfdc0e14d945e04118393631f655647df2155
Emma Karlsson <emma94k@gmail.com>
2023-11-11 10:33:34 +0000

    update urls to saved_posts

--- saved/urls.py
+++ saved/urls.py
@@ -3,5 +3,5 @@ from saved import views
 
 urlpatterns = [
     path('saved_posts/', views.SavedList.as_view()),
-    path('saved/<int:pk>/', views.SavedDetail.as_view()),
+    path('saved_posts/<int:pk>/', views.SavedDetail.as_view()),
 ]
