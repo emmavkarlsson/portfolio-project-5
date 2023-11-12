be6a7c2a3936ee5e57f67fdad336976564a4a43f
Emma Karlsson <emma94k@gmail.com>
2023-11-12 01:06:47 +0000

    add form to Reports create form

--- frontend/src/pages/reports/ReportsCreateForm.js
+++ frontend/src/pages/reports/ReportsCreateForm.js
@@ -7,15 +7,19 @@ import Avatar from "../../components/Avatar";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useCurrentUser } from "../../context/CurrentUserContext";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { Button } from "react-bootstrap";
 
 function ReportsCreateForm() {
     const currentUser = useCurrentUser();
+    const history = useHistory();
 
     const [reportsData, setReportsData] = useState({
         subject: "",
         message: "",
         report_reason: "spam",
     });
+
+    const { subject, message } = reportsData;
 
     useEffect(() => {
         setReportsData((prevState) => ({
@@ -35,7 +39,49 @@ function ReportsCreateForm() {
     };
 
     return (
-        <Form></Form>
+
+        <Form className="mt-2" onSubmit={handleSubmit}>
+
+            <Form.Group>
+                <Form.Label>Why do you wish to report this image?</Form.Label>
+                <select name="report_reason" id="report_reason">
+                    <option value="spam">It's spam</option>
+                    <option value="hate_speech">Hate speech or symbols</option>
+                    <option value="false_information">False information</option>
+                    <option value="bullying_or_harassment">Bullying or harassment</option>
+                    <option value="other">Other reason</option>
+                </select>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Subject</Form.Label>
+                <Form.Control
+                    className={styles.Form}
+                    type="text"
+                    name="subject"
+                    as="textarea"
+                    value={subject}
+                    rows={1}
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Message</Form.Label>
+                <Form.Control
+                    className={styles.Form}
+                    type="text"
+                    name="message"
+                    as="textarea"
+                    value={message}
+                    rows={2}
+                />
+            </Form.Group>
+            <Button
+                className={`${styles.Button} btn d-block ml-auto`}
+                disabled={!subject.trim()}
+                type="submit"
+            >
+                send
+            </Button>
+        </Form>
     );
 }
 
