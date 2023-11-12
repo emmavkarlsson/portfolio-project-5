83571780a076da626794cfbf2f226a660ee3e5ce
Emma Karlsson <emma94k@gmail.com>
2023-11-12 01:19:58 +0000

    update usermessages views to retrieve both sent and received messages

--- usermessages/views.py
+++ usermessages/views.py
@@ -3,17 +3,23 @@ from rest_framework import generics, permissions
 from .models import UserMessage
 from .serializers import UserMessageSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
+from django.db.models import Q
 
 class MessageListCreateView(generics.ListCreateAPIView):
     """
     Let's users create a message
     """
-    queryset = UserMessage.objects.all()
+    queryset = UserMessage.objects.none()
     serializer_class = UserMessageSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return (
+            UserMessage.objects.filter(Q(receiver=self.request.user) | Q(owner=self.request.user)) 
+        )
 
 class MessageDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
