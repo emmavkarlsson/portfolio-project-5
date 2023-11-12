641e72eef64dd19aefede4c39285d2f202b3e0fd
Emma Karlsson <emma94k@gmail.com>
2023-11-12 13:55:34 +0000

    update reportslist to show report status message instead of id

--- frontend/src/pages/reports/ReportsList.js
+++ frontend/src/pages/reports/ReportsList.js
@@ -7,69 +7,75 @@ import Asset from "../../components/Asset";
 import { Col, Row } from "react-bootstrap";
 
 function ReportsList() {
-  const [reportsData, setReportsData] = useState([]);
-  const [hasLoaded, setHasLoaded] = useState(false);
+    const [reportsData, setReportsData] = useState([]);
+    const [hasLoaded, setHasLoaded] = useState(false);
 
-  useEffect(() => {
-    setHasLoaded(false);
-    axiosReq.get("/reports/").then((response) => {
-      if (Array.isArray(response.data.results)) {
-        setReportsData(response.data.results);
-      } else {
-        setReportsData([]);
-      }
-      setHasLoaded(true);
-    });
-  }, []);
+    const REPORT_STATUS = {
+        "awaiting_review": "Awaiting Review",
+        "reviewed": "Reviewed",
+        "closed": "Closed",
+    };
+
+    useEffect(() => {
+        setHasLoaded(false);
+        axiosReq.get("/reports/").then((response) => {
+            if (Array.isArray(response.data.results)) {
+                setReportsData(response.data.results);
+            } else {
+                setReportsData([]);
+            }
+            setHasLoaded(true);
+        });
+    }, []);
 
 
-  return (
-    <Container>
-      {hasLoaded ? (
-        reportsData.length ? (
-          <div>
-            <Row>
-              <Col>
-                <h5>Subject</h5>
-              </Col>
-              <Col>
-                <h5>Message</h5>
-              </Col>
-              <Col>
-                <h5>Status</h5>
-              </Col>
-              {reportsData.some((reports) => reports.is_owner) && (
-                <Col>
-                  <h5>
-                    <i className="fa-regular fa-pen-to-square"></i>
-                  </h5>
-                </Col>
-              )}
-            </Row>
-            {reportsData.map((reports) => (
-              <Row key={reports.id}>
-                <Col>{reports.subject}</Col>
-                <Col>{reports.message}</Col>
-                <Col>{reports.report_status}</Col>
-                {reports.is_owner && (
-                  <Col>
-                    <i className="fa-regular fa-pen-to-square"></i>
-                  </Col>
-                )}
-              </Row>
-            ))}
-          </div>
-        ) : (
-          <p>You haven't reported any pictures.</p>
-        )
-      ) : (
-        <Container className="text-center justify-content-center">
-          <Asset spinner />
-          <p>Loading...</p>
+    return (
+        <Container>
+            {hasLoaded ? (
+                reportsData.length ? (
+                    <div>
+                        <Row>
+                            <Col>
+                                <h5>Subject</h5>
+                            </Col>
+                            <Col>
+                                <h5>Message</h5>
+                            </Col>
+                            <Col>
+                                <h5>Status</h5>
+                            </Col>
+                            {reportsData.some((reports) => reports.is_owner) && (
+                                <Col>
+                                    <h5>
+                                        <i className="fa-regular fa-pen-to-square"></i>
+                                    </h5>
+                                </Col>
+                            )}
+                        </Row>
+                        {reportsData.map((reports) => (
+                            <Row key={reports.id}>
+                                <Col>{reports.subject}</Col>
+                                <Col>{reports.message}</Col>
+                                <Col>{REPORT_STATUS[reports.report_status]}</Col>
+                                {reports.is_owner && (
+                                    <Col>
+                                        <i className="fa-regular fa-pen-to-square"></i>
+                                    </Col>
+                                )}
+                            </Row>
+                        ))}
+                    </div>
+                ) : (
+                    <p>You haven't reported any pictures.</p>
+                )
+            ) : (
+                <Container className="text-center justify-content-center">
+                    <Asset spinner />
+                    <p>Loading...</p>
+                </Container>
+            )}
         </Container>
-      )}
-    </Container>
-  );
+    );
 }
 
 export default ReportsList;
