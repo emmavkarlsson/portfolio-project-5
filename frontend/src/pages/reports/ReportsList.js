292b5347e32deffe82143417d57bb1d84cb050be
Emma Karlsson <emma94k@gmail.com>
2023-11-12 13:54:18 +0000

    add reports list that let's user view their sent reports

---
+++ frontend/src/pages/reports/ReportsList.js
@@ -0,0 +1,75 @@
+// React imports
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+
+import { axiosReq } from "../../api/axiosDefaults";
+import Asset from "../../components/Asset";
+import { Col, Row } from "react-bootstrap";
+
+function ReportsList() {
+  const [reportsData, setReportsData] = useState([]);
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  useEffect(() => {
+    setHasLoaded(false);
+    axiosReq.get("/reports/").then((response) => {
+      if (Array.isArray(response.data.results)) {
+        setReportsData(response.data.results);
+      } else {
+        setReportsData([]);
+      }
+      setHasLoaded(true);
+    });
+  }, []);
+
+
+  return (
+    <Container>
+      {hasLoaded ? (
+        reportsData.length ? (
+          <div>
+            <Row>
+              <Col>
+                <h5>Subject</h5>
+              </Col>
+              <Col>
+                <h5>Message</h5>
+              </Col>
+              <Col>
+                <h5>Status</h5>
+              </Col>
+              {reportsData.some((reports) => reports.is_owner) && (
+                <Col>
+                  <h5>
+                    <i className="fa-regular fa-pen-to-square"></i>
+                  </h5>
+                </Col>
+              )}
+            </Row>
+            {reportsData.map((reports) => (
+              <Row key={reports.id}>
+                <Col>{reports.subject}</Col>
+                <Col>{reports.message}</Col>
+                <Col>{reports.report_status}</Col>
+                {reports.is_owner && (
+                  <Col>
+                    <i className="fa-regular fa-pen-to-square"></i>
+                  </Col>
+                )}
+              </Row>
+            ))}
+          </div>
+        ) : (
+          <p>You haven't reported any pictures.</p>
+        )
+      ) : (
+        <Container className="text-center justify-content-center">
+          <Asset spinner />
+          <p>Loading...</p>
+        </Container>
+      )}
+    </Container>
+  );
+}
+
+export default ReportsList;
