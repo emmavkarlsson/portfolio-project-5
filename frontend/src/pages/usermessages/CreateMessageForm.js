0139b1b7e150f1430c3ed680ca5839a1d83c2b8e
Emma Karlsson <emma94k@gmail.com>
2023-11-13 20:40:49 +0000

    add success alert to createmessageform

--- frontend/src/pages/usermessages/CreateMessageForm.js
+++ frontend/src/pages/usermessages/CreateMessageForm.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import styles from "../../styles/ProfilePage.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
 import Form from "react-bootstrap/Form";
 import { axiosReq } from "../../api/axiosDefaults";
 import { Button } from "react-bootstrap";
+import { AlertContext } from "../../context/AlertContext";
 
 function CreateMessageForm({ profile_id }) {
 
     const [content, setContent] = useState("");
+    const { setAlert } = useContext(AlertContext);
 
     const handleChange = (event) => {
         setContent(event.target.value);
@@ -22,6 +24,7 @@ function CreateMessageForm({ profile_id }) {
                 receiver: profile_id,
             });
             setContent("");
+            setAlert("Your message has been sent!");
         } catch (err) {
             console.log(err);
         }
