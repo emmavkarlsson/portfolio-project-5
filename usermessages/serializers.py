f070f848223b53aba57a80091d3b4b64da3d3832
Emma Karlsson <emma94k@gmail.com>
2023-11-13 15:49:23 +0000

    update usermessage serializer to include profile id and image

--- usermessages/serializers.py
+++ usermessages/serializers.py
@@ -4,6 +4,8 @@ from .models import UserMessage
 class UserMessageSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -11,4 +13,4 @@ class UserMessageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserMessage
-        fields = ['id', 'owner', 'is_owner', 'receiver', 'content', 'created_at']
+        fields = ['id', 'owner', 'is_owner', 'receiver', 'content', 'created_at', 'profile_image', 'profile_id']
