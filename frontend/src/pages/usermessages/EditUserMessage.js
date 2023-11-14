bfa33f78b14fd362a1e5c335844be7682ca8b86b
Emma Karlsson <emma94k@gmail.com>
2023-11-14 23:49:36 +0000

    remove useeffect and update handlesubmit for editusermessages

--- frontend/src/pages/usermessages/EditUserMessage.js
+++ frontend/src/pages/usermessages/EditUserMessage.js
@@ -1,61 +1,40 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Alert from "react-bootstrap/Alert";
 import btnStyles from "../../styles/Button.module.css";
+import { axiosRes } from "../../api/axiosDefaults";
 
-import { useHistory, useParams } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
-import { AlertContext } from "../../context/AlertContext";
+function EditUserMessage(props) {
+    const { id, content, profile_id, setShowEditForm, setUserMessages } = props;
 
-function EditUserMessage() {
-    const [errors, setErrors] = useState({});
-    const { setAlert } = useContext(AlertContext);
-
-    const [userMessageData, setUserMessageData] = useState({
-        content: "",
-    });
-    const { content } = postData;
-    const history = useHistory();
-    const { id } = useParams();
-
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const { data } = await axiosReq.get(userMessageData);
-                const { title, content, image, is_owner } = data;
-
-                is_owner ? setUserMessageData({ content }) : history.push("/");
-            } catch (err) {
-                console.log(err);
-            }
-        };
-
-        handleMount();
-    }, [history, id]);
+    const [formContent, setFormContent] = useState(content);
 
     const handleChange = (event) => {
-        setUserMessageData({
-            ...userMessageData,
-            [event.target.name]: event.target.value,
-        });
+        setFormContent(event.target.value);
     };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const formData = new FormData();
-
-        formData.append("content", content);
-
         try {
-            await axiosReq.put(userMessage, formData);
-            history.goBack();
+            await axiosRes.put(`/usermessages/${id}/`, {
+                receiver: profile_id,
+                content: formContent.trim(),
+            });
+            setUserMessages((prevUserMessages) => ({
+                ...prevUserMessages,
+                results: prevUserMessages.results.map((usermessage) => {
+                    return usermessage.id === id
+                        ? {
+                            ...usermessage,
+                            content: formContent.trim(),
+                        }
+                        : usermessage;
+                }),
+            }));
+            setShowEditForm(false);
         } catch (err) {
             console.log(err);
-            if (err.response?.status !== 401) {
-                setErrors(err.response?.data);
-            }
         }
     };
 
@@ -66,7 +45,7 @@ function EditUserMessage() {
                     type="text"
                     name="content"
                     id="content"
-                    value={content}
+                    value={formContent}
                     onChange={handleChange}
                     placeholder="Type your message..."
                 />
