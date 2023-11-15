4e92b5905c56813a5aebaebc1b9c6cfd02580021
Emma Karlsson <emma94k@gmail.com>
2023-11-15 22:32:08 +0000

    remove console log for comment pages

--- frontend/src/pages/comments/CommentEditForm.js
+++ frontend/src/pages/comments/CommentEditForm.js
@@ -33,9 +33,7 @@ function CommentEditForm(props) {
                 }),
             }));
             setShowEditForm(false);
-        } catch (err) {
-            console.log(err);
-        }
+        } catch (err) {}
     };
 
     return (
