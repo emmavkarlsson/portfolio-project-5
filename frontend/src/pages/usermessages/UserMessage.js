2a28556904fc1590805a5afe27decd96a0d2e91d
Emma Karlsson <emma94k@gmail.com>
2023-11-15 06:33:17 +0000

    add handledelete to usermessages

--- frontend/src/pages/usermessages/UserMessage.js
+++ frontend/src/pages/usermessages/UserMessage.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import styles from "../../styles/Post.module.css";
-import { Card, Media } from "react-bootstrap";
+import { Button, Card, Media, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { useCurrentUser } from '../../context/CurrentUserContext';
 import EditUserMessage from './EditUserMessage';
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { MoreDropdown } from '../../components/MoreDropdown';
+import { axiosRes } from '../../api/axiosDefaults';
 
 const UserMessage = (props) => {
     const {
@@ -21,10 +22,27 @@ const UserMessage = (props) => {
     } = props;
 
     const [showEditForm, setShowEditForm] = useState(false);
+    const [showConfirmDelete, setShowConfirmDelete] = useState(false);
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser.username === owner;
     const is_receiver = currentUser.profile_id === receiver;
+    const history = useHistory();
+
+    const handleDelete = async () => {
+        setShowConfirmDelete(true);
+      };
+    
+      const confirmDelete = async () => {
+        try {
+          await axiosRes.delete(`/usermessages/${id}/`);
+          history.go();
+        } catch (err) {
+          console.log(err);
+        }
+        setShowConfirmDelete(false);
+      };
+    
 
     return (
         <Card className={styles.Post}>
@@ -41,6 +59,7 @@ const UserMessage = (props) => {
                     {is_owner && !showEditForm && (
                         <MoreDropdown
                             handleEdit={() => setShowEditForm(true)}
+                            handleDelete={handleDelete}
                         />
                     )}
                 </Media>
@@ -58,6 +77,26 @@ const UserMessage = (props) => {
                     <>{content && <Card.Text>{content}</Card.Text>}</>
                 )}
             </Card.Body>
+
+            <Modal show={showConfirmDelete} onHide={() => setShowConfirmDelete(false)}>
+        <Modal.Body className="text-center">Are you sure you want to delete?
+        </Modal.Body>
+        <Modal.Footer className="justify-content-center">
+          <Button
+            variant="secondary"
+            onClick={() => setShowConfirmDelete(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="danger"
+            onClick={confirmDelete}
+          >
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
         </Card>
     );
 };
