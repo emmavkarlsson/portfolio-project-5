ee86ac7f1ecfda417f9ac49a843cecc1babba828
Emma Karlsson <emma94k@gmail.com>
2023-11-15 15:54:49 +0000

    update usermessages serializer with receiver fields

--- usermessages/serializers.py
+++ usermessages/serializers.py
@@ -6,6 +6,9 @@ class UserMessageSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    receiver_profile_image = serializers.ReadOnlyField(source='receiver.profile.image.url')
+    receiver_username = serializers.ReadOnlyField(source='receiver.username')
+    receiver_profile_id = serializers.ReadOnlyField(source='receiver.profile.id')
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -13,4 +16,6 @@ class UserMessageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserMessage
-        fields = ['id', 'owner', 'is_owner', 'receiver', 'content', 'created_at', 'profile_image', 'profile_id']
+        fields = ['id', 'owner', 'is_owner', 'receiver',
+        'content', 'created_at', 'profile_image', 'profile_id',
+        'receiver_profile_id', 'receiver_profile_image', 'receiver_username']
