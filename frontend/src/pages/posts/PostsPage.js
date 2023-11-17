2c72eda5ac6d27b5de8ec1388309a1fd286d0f90
Emma Karlsson <emma94k@gmail.com>
2023-11-17 00:38:29 +0000

    add const loggedOutView to PostsPage

--- frontend/src/pages/posts/PostsPage.js
+++ frontend/src/pages/posts/PostsPage.js
@@ -97,12 +97,64 @@ function PostsPage({ message, filter = "" }) {
     </>
   );
 
-  // const loggedOutView = (
+  const loggedOutView = (
+    <>
+      <div className={appStyles.Main}>
+        <Col>
+          <Row className="h-100">
+            <Col>
+              <i className={`fas fa-search ${styles.SearchIcon}`} />
+              <Form
+                className={styles.SearchBar}
+                onSubmit={(event) => event.preventDefault()}
+              >
+                <Form.Control
+                  value={query}
+                  onChange={(event) => setQuery(event.target.value)}
+                  type="text"
+                  className="mr-sm-2"
+                  placeholder="Search posts"
+                />
+              </Form>
+            </Col>
+          </Row>
 
-  // );
+          <Row>
+            <Col>
+              {hasLoaded ? (
+                <>
+                  {posts.results.length ? (
+                    <InfiniteScroll
+                      children={posts.results.map((post) => (
+                        <Post key={post.id} {...post} setPosts={setPosts} />
+                      ))}
+                      dataLength={posts.results.length}
+                      loader={<Asset spinner />}
+                      hasMore={!!posts.next}
+                      next={() => fetchMoreData(posts, setPosts)}
+                    />
+                  ) : (
+                    <Container className={appStyles.Content}>
+                      <Asset src={NoResults} message={message} />
+                    </Container>
+                  )}
+                </>
+              ) : (
+                <Container className={appStyles.Content}>
+                  <Asset spinner />
+                </Container>
+              )}
+            </Col>
+          </Row>
+        </Col>
+      </div>
+    </>
+  );
 
   return (
-    { loggedInView }
+    <div>
+      {loggedInView}
+    </div>
   );
 }
 
