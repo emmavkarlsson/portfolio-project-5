46fee550e8878b510a1d5241fc9895f2f0f326a2
Emma Karlsson <emma94k@gmail.com>
2023-11-18 17:27:02 +0000

    fixed too long line in likes model

--- likes/models.py
+++ likes/models.py
@@ -10,8 +10,10 @@ class Like(models.Model):
     'unique_together' makes sure a user can't like the same post twice.
     """
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name="likes", on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name="likes", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
