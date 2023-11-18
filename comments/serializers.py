aa55fd9ad699c020ee568b87138f7b62880d611b
Emma Karlsson <emma94k@gmail.com>
2023-11-18 11:55:00 +0000

    reformat comments files with black

--- comments/serializers.py
+++ comments/serializers.py
@@ -4,15 +4,15 @@ from comments.models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
+    profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
-        request = self.context['request']
+        request = self.context["request"]
         return request.user == obj.owner
 
     def get_created_at(self, obj):
@@ -24,8 +24,15 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'post', 'created_at', 'updated_at', 'content'
+            "id",
+            "owner",
+            "is_owner",
+            "profile_id",
+            "profile_image",
+            "post",
+            "created_at",
+            "updated_at",
+            "content",
         ]
 
 
@@ -33,4 +40,5 @@ class CommentDetailSerializer(CommentSerializer):
     """
     Serializer for comment model in detail view
     """
-    post = serializers.ReadOnlyField(source='post.id')
+
+    post = serializers.ReadOnlyField(source="post.id")
--- comments/views.py
+++ comments/views.py
@@ -9,11 +9,12 @@ class CommentList(generics.ListCreateAPIView):
     """
     List comments or create a comment if logged in.
     """
+
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['post']
+    filterset_fields = ["post"]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -23,6 +24,7 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve a comment, or update or delete it by id if you own it.
     """
+
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
